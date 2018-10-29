36420f14e1f279d08d65b583a4fa6cf37af093a7
Shoshin Nikita <shoshin.nikita@gmail.com>
2018-10-30 00:37:50 +0300

    Add components SearchTag and SuggestionTag

---
+++ src/components/SearchTag.vue
@@ -0,0 +1,35 @@
+<template>
+	<div
+		class="tag"
+		:style="{ 'background-color': tag.color }"
+		@mouseenter="show = true;"
+		@mouseleave="show = false;"
+	>
+		<div>{{tag.name}}</div>
+		<i
+			class="material-icons"
+			style="cursor: pointer; font-size: 20px;"
+			v-show="show"
+			@click="deleteTagFromSearch();"
+		>close</i>
+	</div>
+</template>
+
+<script>
+export default {
+    name: "search-tag",
+    props: {
+        tag: Object
+    },
+    data: function() {
+        return {
+            show: false
+        };
+    },
+    methods: {
+        deleteTagFromSearch: function() {
+            this.$parent.input().tags.delete(this.tag.id);
+        }
+    }
+};
+</script>
---
+++ src/components/SuggestionTag.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="suggestion-tag" @click="add">
+		<div :style="{ 'background-color': tag.color }" class="tag" style="margin: 0; cursor: pointer;" :title="'id - ' + tag.id">
+			{{tag.name}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+    props: {
+        tag: Object
+    },
+    methods: {
+        add: function() {
+            this.$parent.input().tags.add(this.tag.id);
+        }
+    }
+};
+</script>
+
+<style>
+.suggestion-tag {
+    border-radius: 5px;
+    cursor: pointer;
+    padding: 4px;
+}
+
+.suggestion-tag:hover {
+    background-color: var(--secondary-element-color);
+}
+</style>
