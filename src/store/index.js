d991b9d2dd03fdf1dc78fef7248be7481efa4038
Shoshin Nikita <shoshin.nikita@gmail.com>
2018-10-30 00:38:27 +0300

    Update main.css, fix store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        allFiles: [],
-        allTags: []
+        allFiles: {},
+        allTags: {}
     },
     mutations: {
         updateFiles(state) {
