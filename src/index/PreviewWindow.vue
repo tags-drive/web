01d6eec74b5aea1f674dd76b8eb89612d04eb4c3
Shoshin Nikita <shoshin.nikita@gmail.com>
2018-12-01 22:31:59 +0300

    Preview Window closes on Escape press

--- src/index/PreviewWindow.vue
+++ src/index/PreviewWindow.vue
@@ -315,6 +315,9 @@ export default {
                 case "ArrowLeft":
                     this.previousPreview();
                     break;
+                case "Escape":
+                    this.window().hide();
+                    break;
             }
         }
     }
