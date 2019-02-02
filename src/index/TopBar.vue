798707f35a5b554de4a3156c68bad2061a5cdfc7
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-02-02 19:06:20 +0300

    [WIP] remove methods for custom input #31

--- src/index/TopBar.vue
+++ src/index/TopBar.vue
@@ -152,8 +152,6 @@ import { Events, EventBus } from "@app/index/eventBus";
 import { isErrorStatusCode, logError, logInfo, isElementInPath } from "@app/index/tools";
 import { Params } from "@app/global";
 
-const fontWidth = 18 * 0.6; // px * em
-
 @Component({
     components: {
         tag: TagComponent,
@@ -318,90 +316,6 @@ export default class TopBar extends Vue {
                     .catch(err => logError(err));
             }
         };
-    }
-
-    changeCursorPosition(event: MouseEvent) {
-        let x = event.offsetX > 0 ? event.offsetX : 0;
-        let pos = Math.round(x / fontWidth);
-        this.position = pos < this.expression.length ? pos : this.expression.length;
-    }
-
-    addTagID(id: number) {
-        let text = String(id);
-        this.expression = this.expression.substr(0, this.position) + text + this.expression.substr(this.position);
-        this.position += text.length;
-    }
-
-    addListener() {
-        document.addEventListener("keydown", this.onkeydownListener);
-
-        // Paste event
-        const elem: HTMLElement = document.getElementById("expression-input")!;
-        elem.onpaste = ev => {
-            let text = ev.clipboardData.getData("Text");
-            this.expression = this.expression.substr(0, this.position) + text + this.expression.substr(this.position);
-            this.position += text.length;
-        };
-    }
-
-    removeListener() {
-        document.removeEventListener("keydown", this.onkeydownListener);
-    }
-
-    onkeydownListener(ev: KeyboardEvent) {
-        let hasPrefix = (str: string, prefix: string) => {
-            if (str.length < prefix.length) {
-                return false;
-            }
-
-            for (let i = 0; i < prefix.length; i++) {
-                if (str[i] !== prefix[i]) {
-                    return false;
-                }
-            }
-            return true;
-        };
-
-        if (ev.ctrlKey || ev.altKey) {
-            return;
-        }
-
-        if (
-            ev.key.length === 1 &&
-            (hasPrefix(ev.code, "Key") ||
-                ev.key === "(" ||
-                ev.key === ")" ||
-                ev.key === "|" ||
-                hasPrefix(ev.code, "Numpad") ||
-                hasPrefix(ev.code, "Digit"))
-        ) {
-            this.expression = this.expression.substr(0, this.position) + ev.key + this.expression.substr(this.position);
-
-            this.position++;
-        } else if (hasPrefix(ev.code, "Arrow")) {
-            // Arrow
-            if (hasPrefix(ev.code, "ArrowLeft")) {
-                if (this.position > 0) {
-                    this.position--;
-                }
-            } else if (hasPrefix(ev.code, "ArrowRight")) {
-                if (this.position < this.expression.length) {
-                    this.position++;
-                }
-            }
-        } else if (hasPrefix(ev.code, "Backspace")) {
-            if (this.position === 0) {
-                return;
-            }
-            this.expression = this.expression.substr(0, this.position - 1) + this.expression.substr(this.position);
-            this.position--;
-        } else if (hasPrefix(ev.code, "Delete")) {
-            this.expression = this.expression.substr(0, this.position) + this.expression.substr(this.position + 1);
-        } else if (ev.code === "Home") {
-            this.position = 0;
-        } else if (ev.code === "End") {
-            this.position = this.expression.length;
-        }
     }
 }
 </script>
