60ee30d20284e0c4893d8223b99a98d98508abb7
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-03-17 14:42:54 +0300

    [WIP] add mocks for Search Bar and Files block #25

---
+++ src/mobile/Files.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="wrapper">
+	<div id="all-files">
+		<div
+			v-for="(file, index) in allFiles"
+			:key="index"
+		>
+			<file :file="file"></file>
+		</div>
+	</div>
+</div>
+</template>
+
+
+<style lang="scss" scoped>
+#wrapper {
+    height: calc(100vh - 41px);
+}
+
+#all-files {
+    overflow-y: auto;
+    height: 100%;
+    width: 100%;
+}
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+// Components
+import FileComponent from "@app/mobile/components/File.vue";
+// Other
+import { File } from "@app/global/classes";
+import SharedStore from "@app/mobile/store";
+
+export default Vue.extend({
+    data: function() {
+        return {
+            Store: SharedStore.state
+        };
+    },
+    components: { file: FileComponent },
+    computed: {
+        allFiles: function() {
+            // reactive
+            let reactive = this.Store.allFilesChangesCounter;
+            return this.Store.allFiles;
+        }
+    }
+});
+</script>
---
+++ src/mobile/SearchBar.vue
@@ -0,0 +1,31 @@
+<template>
+	<div
+		id="search-bar"
+		:style="searchBarStyles"
+	>
+		<span>Search Bar (TODO)</span>
+	</div>
+</template>
+
+
+<style lang="scss">
+#search-bar {
+    border: 1px solid #0000002f;
+    height: 40px;
+    position: relative;
+    text-align: center;
+}
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    computed: {
+        searchBarStyles: function() {
+            return {};
+        }
+    }
+});
+</script>
