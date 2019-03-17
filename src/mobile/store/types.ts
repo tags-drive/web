23b78b1ae2308bd3334228fd42f7ea25241de193
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-03-17 14:04:10 +0300

    [WIP] add Store #25

---
+++ src/mobile/store/types.ts
@@ -0,0 +1,11 @@
+import { Tag, File } from "@app/global/classes";
+
+// Vue.js doesn't support reactive Map and Set, so we need to use special counter to
+// update data reactively (https://github.com/vuejs/vue/issues/2410#issuecomment-318487855)
+export interface Store {
+    allFiles: File[];
+    allFilesChangesCounter: number;
+    //
+    allTags: Map<number, Tag>;
+    allTagsChangesCounter: number;
+}
