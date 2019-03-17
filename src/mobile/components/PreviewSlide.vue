112849876b4d2f496042c4e74f48daaf26c18df3
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-03-17 19:45:43 +0300

    [WIP] add PreviewSlide component #25

---
+++ src/mobile/components/PreviewSlide.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		{{ file.filename }}
+	</div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+// Classes and types
+import { File } from "@app/global/classes";
+
+export default Vue.extend({
+    props: {
+        file: File
+    }
+});
+</script>
+
