00a0ae9952dfce21565c703af37331e1ffe81185
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-03-17 14:44:29 +0300

    [WIP] update index.ts #25

--- src/mobile/index.ts
+++ src/mobile/index.ts
@@ -1,5 +1,20 @@
 import Vue from "vue";
+//
+import TagsDrive from "./TagsDrive.vue";
+import SearchBar from "./SearchBar.vue";
+import Files from "./Files.vue";
 
 new Vue({
-    el: "#tags-drive"
+    el: "#tags-drive",
+    render: h => h(TagsDrive)
+});
+
+new Vue({
+    el: "#search-bar",
+    render: h => h(SearchBar)
+});
+
+new Vue({
+    el: "#all-files-list",
+    render: h => h(Files)
 });
