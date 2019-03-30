e6339cdd7912f8640abaf38d9a3d248d4ec0dfd0
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-03-30 11:42:32 +0300

    [WIP] refactor components/File/File.vue

--- src/index/components/File/File.vue
+++ src/index/components/File/File.vue
@@ -109,8 +109,6 @@
 
 <script lang="ts">
 import Vue from "vue";
-import Component from "vue-class-component";
-import { Prop } from "vue-property-decorator";
 // Components
 import TagComponent from "@components/Tag/Tag.vue";
 // Classes and types
@@ -130,150 +128,151 @@ import dateformat from "dateformat";
 const tagsListPadding = 4;
 const sizeSuffixes: string[] = ["B", "KB", "MB", "GB", "TB"];
 
-@Component({
-    components: {
-        tag: TagComponent
-    }
-})
-export default class extends Vue {
-    @Prop() file!: TableFile;
-    overflow: boolean = false;
-    tagsListHover: boolean = false;
-    // rightClicked is true when user clicked right button to show context menu
-    // It is became false after hiding Context Menu (when Events.ContextMenu.Hide emitted)
-    rightClicked: boolean = false;
-    // setInterval id
-    overflowChecker: number = -1;
-    //
-    readonly Store: Store = SharedStore.state;
-    readonly State: State = SharedState.state;
-
-    get stylesObject() {
-        let style = <any>{
-            opacity: this.file.deleted && !this.file.selected ? 0.4 : 1
+export default Vue.extend({
+    props: {
+        file: TableFile
+    },
+    data: function() {
+        return {
+            overflow: false,
+            tagsListHover: false,
+            // rightClicked is true when user clicked right button to show context menu
+            // It is became false after hiding Context Menu (when Events.ContextMenu.Hide emitted)
+            rightClicked: false,
+            // setInterval id
+            overflowChecker: -1,
+            //
+            Store: SharedStore.state,
+            State: SharedState.state
         };
+    },
+    computed: {
+        stylesObject: function() {
+            let style = <any>{
+                opacity: this.file.deleted && !this.file.selected ? 0.4 : 1
+            };
 
-        if (this.file.selected || this.rightClicked) {
-            style["background-color"] = "#dcdcdcc0";
-        }
-
-        return style;
-    }
-
-    get titleMessage(): string {
-        if (!this.file.deleted) {
-            return "";
-        }
-        let date = new Date(this.file.timeToDelete),
-            now = new Date();
-
-        let leftHours = Math.round((date.getTime() - now.getTime()) / (1000 * 60 * 60)), // in hours
-            leftDays = 0;
-
-        if (leftHours < 1) {
-            // File should be already deleted or it's left less than 1 hour
-            return "File is in a Trash. It will be deleted soon";
-        }
-
-        if (leftHours > 24) {
-            leftDays = Math.round(leftHours / 24);
-            leftHours = leftHours % 24;
-        }
-
-        return `File is in a Trash. It will be deleted in ${leftDays} days ${leftHours} hours`;
-    }
-
-    get previewLink(): string {
-        if (this.file.type.previewType === Const.previewTypes.image) {
-            return Params.Host + "/" + this.file.preview;
-        }
-
-        return Params.Host + "/ext/" + this.file.filename.split(".").pop();
-    }
-
-    get tagsStyle() {
-        if (!this.tagsListHover && this.overflow) {
-            return { "border-left-color": "black" };
-        }
-
-        let list = <HTMLElement>this.$refs["tags-list"];
-        if (list === undefined || list.clientHeight >= list.scrollHeight) {
-            return;
-        }
-
-        let wrapper = <HTMLElement>this.$refs["tags-list-wrapper"];
-        if (wrapper === undefined) {
-            return;
-        }
-
-        // Magic //
-
-        // There is a bug with RecycleScroller when next files cover expanded tags-list.
-        // So we have to change their z-index to n-1 and current to n (n = 1).
-
-        // Get current RecycleScroller node
-        let currNode = list.parentElement!.parentElement!.parentElement!.parentElement!;
-        currNode.style.zIndex = "1";
-
-        let RecycleScroller = currNode.parentElement!;
-        let shouldChange = false;
-        for (let i = 0; i < RecycleScroller.childNodes.length; i++) {
-            if (RecycleScroller.childNodes[i] === currNode) {
-                shouldChange = true;
-                continue;
+            if (this.file.selected || this.rightClicked) {
+                style["background-color"] = "#dcdcdcc0";
             }
 
-            if (shouldChange) {
-                let nextNode = <HTMLElement>RecycleScroller.childNodes[i];
-                if (nextNode !== undefined) {
-                    nextNode.style.zIndex = "0";
+            return style;
+        },
+        titleMessage: function(): string {
+            if (!this.file.deleted) {
+                return "";
+            }
+            let date = new Date(this.file.timeToDelete),
+                now = new Date();
+
+            let leftHours = Math.round((date.getTime() - now.getTime()) / (1000 * 60 * 60)), // in hours
+                leftDays = 0;
+
+            if (leftHours < 1) {
+                // File should be already deleted or it's left less than 1 hour
+                return "File is in a Trash. It will be deleted soon";
+            }
+
+            if (leftHours > 24) {
+                leftDays = Math.round(leftHours / 24);
+                leftHours = leftHours % 24;
+            }
+
+            return `File is in a Trash. It will be deleted in ${leftDays} days ${leftHours} hours`;
+        },
+        previewLink: function(): string {
+            if (this.file.type.previewType === Const.previewTypes.image) {
+                return Params.Host + "/" + this.file.preview;
+            }
+
+            return Params.Host + "/ext/" + this.file.filename.split(".").pop();
+        },
+        tagsStyle: function() {
+            if (!this.tagsListHover && this.overflow) {
+                return { "border-left-color": "black" };
+            }
+
+            let list = <HTMLElement>this.$refs["tags-list"];
+            if (list === undefined || list.clientHeight >= list.scrollHeight) {
+                return;
+            }
+
+            let wrapper = <HTMLElement>this.$refs["tags-list-wrapper"];
+            if (wrapper === undefined) {
+                return;
+            }
+
+            // Magic //
+
+            // There is a bug with RecycleScroller when next files cover expanded tags-list.
+            // So we have to change their z-index to n-1 and current to n (n = 1).
+
+            // Get current RecycleScroller node
+            let currNode = list.parentElement!.parentElement!.parentElement!.parentElement!;
+            currNode.style.zIndex = "1";
+
+            let RecycleScroller = currNode.parentElement!;
+            let shouldChange = false;
+            for (let i = 0; i < RecycleScroller.childNodes.length; i++) {
+                if (RecycleScroller.childNodes[i] === currNode) {
+                    shouldChange = true;
+                    continue;
+                }
+
+                if (shouldChange) {
+                    let nextNode = <HTMLElement>RecycleScroller.childNodes[i];
+                    if (nextNode !== undefined) {
+                        nextNode.style.zIndex = "0";
+                    }
                 }
             }
+            // End of Magic //
+
+            let rect = wrapper.getBoundingClientRect();
+
+            return {
+                "background-color": "white",
+                border: "1px solid #00000080",
+                "border-radius": "5px",
+                height: "auto",
+                left: rect.left - 1 + "px", // minus border width
+                padding: tagsListPadding + "px",
+                position: "fixed",
+                width: rect.width - tagsListPadding * 2 + "px"
+            };
+        },
+        fileSize: function(): string {
+            let suffixIndex = 0;
+            // In bytes
+            let size = this.file.size;
+            while (size / 1024 > 1) {
+                size /= 1024;
+                suffixIndex++;
+            }
+
+            if (suffixIndex >= sizeSuffixes.length) {
+                return "-";
+            }
+
+            let s = size.toFixed(1);
+            if (s[s.length - 1] == "0") {
+                // Trim trailing dot and zero
+                s = s.slice(0, s.length - 2);
+            }
+
+            return s + " " + sizeSuffixes[suffixIndex];
+        },
+        fileAddTime(): string {
+            // Example: "Mar 6, 2019 14:50"
+            return dateformat(this.file.addTime, "mmm d, yyyy HH:MM");
         }
-        // End of Magic //
-
-        let rect = wrapper.getBoundingClientRect();
-
-        return {
-            "background-color": "white",
-            border: "1px solid #00000080",
-            "border-radius": "5px",
-            height: "auto",
-            left: rect.left - 1 + "px", // minus border width
-            padding: tagsListPadding + "px",
-            position: "fixed",
-            width: rect.width - tagsListPadding * 2 + "px"
-        };
-    }
-
-    get fileSize(): string {
-        let suffixIndex = 0;
-        // In bytes
-        let size = this.file.size;
-        while (size / 1024 > 1) {
-            size /= 1024;
-            suffixIndex++;
-        }
-
-        if (suffixIndex >= sizeSuffixes.length) {
-            return "-";
-        }
-
-        let s = size.toFixed(1);
-        if (s[s.length - 1] == "0") {
-            // Trim trailing dot and zero
-            s = s.slice(0, s.length - 2);
-        }
-
-        return s + " " + sizeSuffixes[suffixIndex];
-    }
-
-    get fileAddTime(): string {
-        // Example: "Mar 6, 2019 14:50"
-        return dateformat(this.file.addTime, "mmm d, yyyy HH:MM");
-    }
-
-    created() {
+    },
+    //
+    components: {
+        tag: TagComponent
+    },
+    //
+    created: function() {
         this.overflowChecker = setInterval(() => {
             let list = <HTMLElement>this.$refs["tags-list"];
             if (list === undefined) {
@@ -283,48 +282,47 @@ export default class extends Vue {
 
             this.overflow = list.scrollHeight > list.clientHeight;
         }, 10);
-    }
-
-    destroyed() {
+    },
+    destroyed: function() {
         clearInterval(this.overflowChecker);
-    }
+    },
+    //
+    methods: {
+        showContextMenu(event: MouseEvent) {
+            // Don't show Context Menu when file isn't selected
+            if (this.State.selectMode && !this.file.selected) {
+                return;
+            }
 
-    showContextMenu(event: MouseEvent) {
-        // Don't show Context Menu when file isn't selected
-        if (this.State.selectMode && !this.file.selected) {
-            return;
+            // Reset other files at first
+            EventBus.$emit(Events.FilesBlock.UnfocusFile);
+
+            this.rightClicked = true;
+
+            // Show Context Menu
+            EventBus.$emit(Events.ShowContextMenu, { file: this.file, x: event.x, y: event.y });
+
+            // handler sets rightClicked to false and unregisters itself from EventBus
+            let handler = () => {
+                this.rightClicked = false;
+                EventBus.$off(Events.FilesBlock.UnfocusFile, handler);
+            };
+
+            EventBus.$on(Events.FilesBlock.UnfocusFile, handler);
+        },
+        showPreview() {
+            EventBus.$emit(Events.ShowPreview, { file: this.file });
+        },
+        toggleSelect() {
+            // We can skip changing this.selected, because a checkbox is bound to this.selected
+
+            // The function is called after changing this.selected
+            if (this.file.selected) {
+                EventBus.$emit(Events.FilesBlock.SelectFile, { id: this.file.id });
+            } else {
+                EventBus.$emit(Events.FilesBlock.UnselectFile, { id: this.file.id });
+            }
         }
-
-        // Reset other files at first
-        EventBus.$emit(Events.FilesBlock.UnfocusFile);
-
-        this.rightClicked = true;
-
-        // Show Context Menu
-        EventBus.$emit(Events.ShowContextMenu, { file: this.file, x: event.x, y: event.y });
-
-        // handler sets rightClicked to false and unregisters itself from EventBus
-        let handler = () => {
-            this.rightClicked = false;
-            EventBus.$off(Events.FilesBlock.UnfocusFile, handler);
-        };
-
-        EventBus.$on(Events.FilesBlock.UnfocusFile, handler);
     }
-
-    showPreview() {
-        EventBus.$emit(Events.ShowPreview, { file: this.file });
-    }
-
-    toggleSelect() {
-        // We can skip changing this.selected, because a checkbox is bound to this.selected
-
-        // The function is called after changing this.selected
-        if (this.file.selected) {
-            EventBus.$emit(Events.FilesBlock.SelectFile, { id: this.file.id });
-        } else {
-            EventBus.$emit(Events.FilesBlock.UnselectFile, { id: this.file.id });
-        }
-    }
-}
+});
 </script>
