cdc95b00834745543de86436292ebdbd67fc91cc
Shoshin Nikita <shoshin.nikita@gmail.com>
2019-06-04 16:33:31 +0300

    [WIP] index: detect session state #41
    * Detect if a user is authorized
    * Detect Share Mode

--- src/index/index.ts
+++ src/index/index.ts
@@ -8,6 +8,23 @@ import ContextMenu from "@app/index/ContextMenu.vue";
 import ModalWindow from "@app/index/ModalWindow.vue";
 import LogWindow from "@app/index/LogWindow.vue";
 import PreviewWindow from "@app/index/PreviewWindow.vue";
+//
+import ShareState from "@app/index/state";
+import API from "@app/index/api";
+
+// Define if user is authorized
+API.isUserAuthorized().then(authorized => {
+    if (authorized) {
+        ShareState.commit("setAuthorized");
+    }
+});
+
+// Detect share mode
+let res = /shareToken=([\w\d]+)/g.exec(location.search);
+if (res !== null && res[1] !== undefined) {
+    ShareState.commit("enableShareMode");
+    ShareState.commit("setShareToken", { token: res[1] });
+}
 
 // Tags Drive (main instance)
 new Vue({
